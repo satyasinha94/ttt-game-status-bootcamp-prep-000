bb40c6afa120cc40f62d75e88a031ba1a90e36c9
beneficial-bundle-8122 <satyasinha94@gmail.com>
2018-10-02 03:53:10 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -61,7 +61,7 @@ def winner(board)
     position_1 = board[win_index_1]
     position_2 = board[win_index_2]
     position_3 = board[win_index_3]
-     if position_1 == "X" && position_2 == "X" && position_3 == "X" 
+     if position_1 == "X" && position_2 == "X" && position_3 == "X"
       return "X"
     elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
       return "O"
